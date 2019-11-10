8c01ecb2cd23f710548da0e5b06e90e91b3eea67
rasel003 <rasel.00x@gmail.com>
2019-11-10 15:09:17 +0600

    refactored to androidx

--- app/src/main/java/com/example/rasel/calculator/Main2Activity.java
+++ app/src/main/java/com/example/rasel/calculator/Main2Activity.java
@@ -2,7 +2,7 @@ package com.example.rasel.calculator;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
